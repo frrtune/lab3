ad4986ba533d9433f3395498417f5495bce1e7d2
Daria <bdrdria@yandex.ru>
2025-11-27 21:32:11 +0300

    feat: add definition of deque

---
+++ src/containers/deque.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <stddef.h>
+
+typedef struct Node {
+    void* data;
+    struct Node* prev;
+    struct Node* next;
+} node;
+
+typedef struct Deque {
+    node* head;
+    node* tail;
+    size_t size;
+} deque;
