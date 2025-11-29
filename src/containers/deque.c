393485e769b8d3569863de2859a8bc127e2b103c
Daria <bdrdria@yandex.ru>
2025-11-29 18:46:02 +0300

    feat: add deleting element from the beginning, end & middle of deque

--- src/containers/deque.c
+++ src/containers/deque.c
@@ -81,9 +81,11 @@ void deque_add_middle(deque* d, void* data, size_t index){
     if (d == NULL || index >= d->size) return;
     if (index == 0) {
         deque_add_begin(d, data);
+        return;
     }
     if (index == d->size) {
         deque_add_end(d, data);
+        return;
     }
 
     node* current_node = d->head;
@@ -97,4 +99,51 @@ void deque_add_middle(deque* d, void* data, size_t index){
     current_node->prev->next = new_node;
     current_node->prev = new_node;
     d->size++;
+}
+
+void deque_del_begin(deque* d){
+    if (d == NULL || d->head == NULL) return;
+    node* old_head = d->head;
+    d->head = old_head->next;
+    if (d->head != NULL){
+        d->head->prev = NULL;
+    } else {
+        d->tail = NULL;
+    }
+    free(old_head);
+    d->size--;
+}
+
+void deque_del_end(deque* d){
+    if (d == NULL || d->tail == NULL) return;
+    node* old_tail = d->tail;
+    d->tail = old_tail->prev;
+    if (d->tail != NULL){
+        d->tail->next = NULL;
+    } else {
+        d->head = NULL;
+    }
+    free(old_tail);
+    d->size--;
+}
+
+void deque_del_middle(deque* d, size_t index){
+    if (d == NULL || index >= d->size) return;
+    if (index == 0) {
+        deque_del_begin(d);
+        return;
+    }
+    if (index == d->size - 1) {
+        deque_del_end(d);
+        return;
+    }
+
+    node* current_node = d->head;
+    for (size_t i = 0; i < index; i++){
+        current_node = current_node->next;
+    }
+    current_node->prev->next = current_node->next;
+    current_node->next->prev = current_node->prev;
+    free(current_node);
+    d->size--;
 }
--- src/containers/deque.h
+++ src/containers/deque.h
@@ -23,3 +23,6 @@ node* deque_prev(node* n);
 void deque_add_begin(deque* d, void* data);
 void deque_add_end(deque* d, void* data);
 void deque_add_middle(deque* d, void* data, size_t index);
+void deque_del_begin(deque* d);
+void deque_del_end(deque* d);
+void deque_del_middle(deque* d, size_t index);
