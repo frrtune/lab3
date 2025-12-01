1fcb9b03db90081362f358d0bc230f30914d8045
Daria <bdrdria@yandex.ru>
2025-12-01 23:38:52 +0300

    fix: change name of function

--- src/containers/deque.h
+++ src/containers/deque.h
@@ -14,7 +14,18 @@ typedef struct Deque {
     size_t size;
 } deque;
 
+/**
+ * @brief Инициализация двухсторонней очереди
+ * 
+ * @return deque* 
+ */
 deque* deque_init();
+/**
+ * @brief Получение размера двухсторонней очереди
+ * 
+ * @param d 
+ * @return size_t 
+ */
 size_t deque_get_size(const deque* d);
 void* deque_get_elem(deque* d, size_t index);
 node* deque_begin(deque* d);
@@ -23,7 +34,7 @@ node* deque_next(node* n);
 node* deque_prev(node* n);
 void deque_push_front(deque* d, void* data);
 void deque_push_back(deque* d, void* data);
-void deque_push_insert(deque* d, void* data, size_t index);
+void deque_insert(deque* d, void* data, size_t index);
 void deque_del_begin(deque* d);
 void deque_del_end(deque* d);
 void deque_del_middle(deque* d, size_t index);
