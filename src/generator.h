812ef4cd0f8e7c0c49d012022eacfe06ee0e89ac
Daria <bdrdria@yandex.ru>
2025-12-03 11:35:31 +0300

    docs: add documentation

--- src/generator.h
+++ src/generator.h
@@ -2,6 +2,13 @@
 
 #include "containers/deque.h"
 
+/**
+ * @brief Генерирует "дома" с рандомными параметрами
+ * 
+ * @param d 
+ * @param n количество домов
+ * @return int 0 - успех, 1- ошибка
+ */
 int generate_house(deque* d, int n);
 
     
--- src/io.h
+++ src/io.h
@@ -1,5 +1,26 @@
 #pragma once
 
+/**
+ * @brief Читает данные из CSV файла, генерирует "дома" и записывает их в двухстороннюю очередь
+ * 
+ * @param file_name 
+ * @param deque 
+ * @return int 0 - успех, 1 - ошибка
+ */
 int read_csv(const char* file_name, deque* deque);
+/**
+ * @brief Записывает элементы двухсторонней очереди ("дома") в виде строк в CSV файл
+ * 
+ * @param file_name 
+ * @param deque 
+ * @return int 
+ */
 int write_csv(const char* file_name, deque* deque);
+/**
+ * @brief Создает таблицу с фиксированной шириной с данными о "домах"
+ * 
+ * @param file_name 
+ * @param d 
+ * @return int 
+ */
 int print_table(const char* file_name, deque* d);
