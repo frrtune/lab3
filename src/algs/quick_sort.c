f7e83d481ced0a658317c45795fc2ac2e728141d
Daria <bdrdria@yandex.ru>
2025-12-22 14:37:57 +0300

    feat: add partition function for quick sort

--- src/algs/quick_sort.c
+++ src/algs/quick_sort.c
@@ -21,3 +21,18 @@ static int get_median(deque* d, cmpr compare, int int_low, int int_high) {
         return int_middle;
     }
 }
+
+static int partition(deque* d, cmpr compare, int int_low, int int_high) {
+    int median_index = get_median(d, compare, int_low, int_high);
+    void* pivot = deque_get_elem(d, median_index);
+    int i = int_low;
+    int j = int_high;
+    while(1){
+        while (compare(deque_get_elem(d, i), pivot) < 0) i++;
+        while (compare(deque_get_elem(d, j), pivot) > 0) j--;
+        if (i >= j) return j;
+        deque_swap(d, i, j);
+        i++;
+        j--;
+    }
+}
